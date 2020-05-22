19f473012ae92b51cce635334e73607fb451511e
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-05-22 14:16:44 -0400

    Initial Commit

---
+++ scripts/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+########################################################
+#                                                      #
+#         For Dev info, please contact me:             #
+#         https://riverside.rocks/contact              #
+#                                                      #
+#           Copyright 2020 Trent Wiles                 #
+#          For lisence please see LISENCE              #
+#                                                      #
+########################################################
+
+
+echo "Riverside Rocks Mail Devivery SubSystem";
+echo " ";
+echo "For copyright and more please visit us on GitHub, github.com/riversiderocks/wdss";
